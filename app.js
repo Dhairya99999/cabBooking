f958d1d0b983ab4748cb6817c86af34d6854ccd7
Dhairya99999 <dhairya.jan9@gmail.com>
2024-08-26 11:34:19 +0530

    socket

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import http from 'http';
-import { WebSocketServer } from 'ws';
-import dotenv from "dotenv";
-import connectDB from "./constants/db.js";  
+import { Server as SocketIOServer } from 'socket.io';
+import dotenv from 'dotenv';
+import connectDB from './constants/db.js';  
 
 dotenv.config();
 
@@ -10,19 +10,16 @@ import userRouter from './routes/user.route.js';
 import rideRoutes from './routes/cab.routes.js'; 
 import cabRoutes from './routes/cab.routes.js';
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
 
-
 app.use(express.json());
 
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
-
+const io = new SocketIOServer(server);
 
 // Root Route
 app.use('/', (req, res, next) => {
@@ -36,15 +33,21 @@ app.use('/user', userRouter);
 app.use('/rides', rideRoutes); 
 app.use('/cab', cabRoutes);
 
-server.listen(port,'0.0.0.0', () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// WebSocket connection handling
-wss.on('connection', (ws) => {
+// Socket.IO connection handling
+io.on('connection', (socket) => {  
   console.log('New client connected');
 
-  ws.on('close', () => {
+  // Example event handler
+  socket.on('message', (data) => {
+    console.log('Received exampleEvent with data:', data);
+    socket.emit('responseEvent', { message: 'Data received' });
+  });
+
+  socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
